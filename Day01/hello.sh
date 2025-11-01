bf828c97f2e2c8d6d89e34788349564b8d9bb5ff
Karthik H <h.karthiknair@gmail.com>
2025-11-01 23:51:35 +0530

    first shell script

---
+++ Day01/hello.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# hash represents comment in shell scripting
+#  the first line: #!/bin/bash is called shebang or hashbang
+#  it tells the system which interpreter should execute the script.
+
+
+# down below is the first script
+
+echo "this is the first script"
+echo "this is the second script"
+echo "this is the third script"
+
+
